8cfb7920eb888aebccc5d3ed7db9617f58ad0481
William Guss <carlostnb@gmail.com>
2018-01-23 13:17:43 -0500

    Added action execution for behavioral cloning.

--- src/tests/behavioral_cloning/cloner.py
+++ src/tests/behavioral_cloning/cloner.py
@@ -24,7 +24,8 @@ from config import(
 from config import (
     SMALLEST_FEATURE_MAP,
     FC_SIZES,
-    LEARNING_RATE)
+    LEARNING_RATE,
+    DROPOUT)
 
 def get_options():
     parser = argparse.ArgumentParser(description='Clone some expert data..')
@@ -63,9 +64,9 @@ class Agent:
         Returns: state_placeholder, action output tensor.
         """
         state_ph = tf.placeholder(tf.float32, shape=[None] + self.state_space)
-        training_ph = tf.placeholder(tf.bool, shape=[1])
+        training_ph = tf.placeholder(tf.bool)
 
-        filter_size = max(self.state_space)
+        filter_size = max(self.state_space[:-1])
         head = state_ph
 
         # Apply some convolutions.
@@ -79,16 +80,18 @@ class Agent:
                     kernel_size=[5, 5],
                     padding="same",
                     activation=tf.nn.relu)
+                filter_size = min(head.get_shape().as_list()[1:-1])
 
-                # Apply pooling
-                head = tf.layers.max_pooling2d(inputs=head, pool_size=[2, 2], strides=2)
+                if filter_size > SMALLEST_FEATURE_MAP:
+                    # Apply pooling
+                    head = tf.layers.max_pooling2d(inputs=head, pool_size=[2, 2], strides=2)
 
             conv_iter += 1
-            filter_size = max(head.get_shape.as_list()[1:])
+            filter_size = min(head.get_shape().as_list()[1:-1])
             
         # Flatten to fully connected
         with tf.variable_scope("flatten"):
-            num_neurons = np.prod(head.get_shape().as_list()[1:], 1)
+            num_neurons = np.prod(head.get_shape().as_list()[1:])
             head = tf.reshape(head, [-1, num_neurons])
 
         # Apply some fc layers
@@ -98,20 +101,25 @@ class Agent:
 
         # Apply dropout
         head = tf.layers.dropout(
-            inputs=head, rate=0.5, training=self.training_ph)
+            inputs=head, rate=DROPOUT, training=training_ph)
 
         with tf.variable_scope("fc_final"):
             # Calculate the dimensionality of action space
             num_outputs = sum(self.action_space)
-            head = tf.layers.dense(inputs=dropout, units=num_outputs)
+            head = tf.layers.dense(inputs=head, units=num_outputs)
 
         # Apply selective softmax accordin to various XOR conditions on the output
         with tf.variable_scope("action"):
             action = []
             subspace_iter = 0
+
             for space in self.action_space:
                 logits = head[:, subspace_iter:subspace_iter+space]
-                action.append((logits, tf.nn.softmax(logits)))
+                # Note: we could also collect probabilities:
+                # probabilities = tf.nn.softmax(logits)
+                argmax = tf.nn.tf.argmax(input=logits, axis=1)
+                
+                action.append((logits, argmax))
                 subspace_iter += space
 
         return state_ph, training_ph, action
@@ -123,19 +131,19 @@ class Agent:
         """
         with tf.variable_scope("training"):
             # Create the label placeholder
-            label_ph = tf.placeholder(tf.int32, shape=[None] + len(self.action_space))
+            label_ph = tf.placeholder(tf.int32, shape=[None, len(self.action_space)])
             sublabel = []
             with tf.variable_scope("label_processing"):
                 # Convert it to a onehot.
                 for i, space in enumerate(self.action_space):
-                    with tf.variable_scope("one_hot_{}".format(i))
-                    sublabel.append(
-                        tf.one_hot(indices=tf.cast(label_ph[:,i], tf.int32), depth=space))
+                    with tf.variable_scope("one_hot_{}".format(i)):
+                        sublabel.append(
+                            tf.one_hot(indices=tf.cast(label_ph[:,i], tf.int32), depth=space))
 
-            # First create the loss
+            # First create the loss for each subspace.
             with tf.variable_scope("loss"):
                 subloss = []
-                for (logit_subspace, _), label) in zip(self.action, sublabel):
+                for ((logit_subspace, _), label) in zip(self.action, sublabel):
                     with tf.variable_scope("subloss_{}".format(len(subloss))):
                         subloss.append(
                             tf.losses.softmax_cross_entropy(
@@ -150,9 +158,50 @@ class Agent:
 
         return label_ph, loss, train_op
 
+    def train(self, batch_states, batch_labels):
+        """
+        Trains the model
+        """
+        cur_loss, _ = self.sess.run([self.loss, self.train_op], {
+            self.training_ph: True,
+            self.state_ph: batch_states,
+            self.label_ph: batch_labels
+            })
+        return cur_loss
+
+    def act(self, state):
+        """
+        Acts on single or multiple states.
+        Returns actions in a onehot 
+        """
+        # Handle shape size update if single state
+        assert len(state.shape) >= 3
+        
+        single_state = len(state.shape) == 3
+        if single_state:
+            state = np.expand_dims(state, axis=0)
+        
+
+        # Feed the state and get the action onehot
+        _, argmax_subspace = zip(*self.action)
+        subspace_action_argmax = self.sess.run(argmax_subspace, {
+            self.state_ph: state,
+            self.training_ph: False
+            })
+
+        # If it's a single state, return a single action
+        # not a list of actions.
+        if single_state:
+            subspace_action_argmax = [
+                subspace[0] for subspace in subspace_action_argmax]
+
+        return subspace_action_argmax
 
 
-def run_training(coord, agent):
+
+
+
+def run_training(coord, agent, bc_data_dir):
     """
     Runs training for the agent.
     """
@@ -184,16 +233,17 @@ def run_main(opts):
     # Define the action space. (length of key bindings plus null action)
     action_space = [len(d) + 1for d,_ in BINDINGS]
     # Define the state space.
-    state_space = list(GYM_RESOLUTION)
-    
+    state_space = list(GYM_RESOLUTION) + [3] # 3 color channels
+
     # Create a model
     sess = tf.InteractiveSession()
     agent = Agent(env, state_space, action_space, sess)
+    sess.run(tf.global_variables_initializer())
 
-    1/0
     # Start the training thread with a coordinator.
     coord = tf.train.Coordinator()
-    training_thread = threading.Thread(target=run_training, args=(coord, agent))
+    training_thread = threading.Thread(
+        target=run_training, args=(coord, agent, opts.bc_data))
     training_thread.start()
 
     # Begin performing demonstrations.
